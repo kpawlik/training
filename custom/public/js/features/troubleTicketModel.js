b0e9468b387a2e41ab2893fa79f274d0dba6ab70
Kris Pawlik <kpawlik78@gmail.com>
2024-09-26 11:23:23 +0200

    tt-model

---
+++ custom/public/js/features/troubleTicketModel.js
@@ -0,0 +1,12 @@
+import myw from 'myWorld-client';
+
+class troubleTicketModel extends myw.MyWorldFeature {
+
+}
+
+myw.featureModels['trouble_ticket'] = troubleTicketModel;
+
+
+export {
+    troubleTicketModel
+}
--- custom/public/js/main.standard.js
+++ custom/public/js/main.standard.js
@@ -1,7 +1,3 @@
 import myw from 'myWorld-client';
 import 'main.standard';                       
-
-const buttons = myw.applicationDefinition.layout.controls.toolbar.options.buttons;
-const printIndex = buttons.indexOf('print.dialog');
-buttons.splice(printIndex);
-delete myw.applicationDefinition.plugins['print'];
+import './features/troubleTicketModel';
