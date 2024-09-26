d18f1a7a694b4c95e33c8464985f094bf2389a42
Kris Pawlik <kpawlik78@gmail.com>
2024-09-26 11:24:06 +0200

    03-tt-readonly fields

--- custom/public/js/features/troubleTicketModel.js
+++ custom/public/js/features/troubleTicketModel.js
@@ -1,7 +1,9 @@
 import myw from 'myWorld-client';
 
 class troubleTicketModel extends myw.MyWorldFeature {
-
+    static {
+        this.prototype.readonlyFields = ['datetime_completed'];
+    }
 }
 
 myw.featureModels['trouble_ticket'] = troubleTicketModel;
